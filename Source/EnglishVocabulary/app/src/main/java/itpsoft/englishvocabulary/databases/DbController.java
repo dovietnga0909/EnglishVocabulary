1404fb5d742d576220c784f7fd8eef091fe9b967
Đinh Thế Luân <luandt1005@gmail.com>
2015-06-03 17:39:39 +0700

    database

--- Source/EnglishVocabulary/app/src/main/java/itpsoft/englishvocabulary/databases/DbController.java
+++ Source/EnglishVocabulary/app/src/main/java/itpsoft/englishvocabulary/databases/DbController.java
@@ -9,13 +9,32 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DbController extends SQLiteOpenHelper {
 
-    public DbController(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static String DB_NAME = "english_vocabulary";
+    private static String DB_PATH = "/data/data/itpsoft.englishvocabulary/databases/";
+    private static int DB_VERSION = 1;
+
+    private static SQLiteDatabase database;
+    private Context context;
+
+    //Table category
+
+
+    public DbController(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        database = getWritableDatabase();
+        context = context;
+    }
+
+    private static DbController mInstance;
+    public static DbController getInstance(Context context){
+        if(mInstance == null)
+            mInstance = new DbController(context);
+        return mInstance;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL();
     }
 
     @Override
