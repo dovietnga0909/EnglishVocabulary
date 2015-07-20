4aaf62721cba8972ce20526d5a3bd2561ed30e49
dovietnga0909 <dovietnga0909@gmail.com>
2015-07-20 21:13:47 +0700

    ngadv add login , register

---
+++ Source/EnglishVocabulary/app/src/main/java/itpsoft/englishvocabulary/LoginActivity.java
@@ -0,0 +1,20 @@
+package itpsoft.englishvocabulary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+/**
+ * Created by Do on 20/07/2015.
+ */
+public class LoginActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //hide keyboard firstTime
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        );
+        setContentView(R.layout.activity_login);
+    }
+}
